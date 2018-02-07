de7f8fc28cbaa73beafe64a854ada1fe75d44fd1
hzsunzixiang <haichengsun123@163.com>
2018-02-07 10:37:30 +0800

    latex Bibliography in LaTeX with Bibtex/Biblatex

---
+++ latex/ctex_study/bibliography.sh
@@ -0,0 +1,7 @@
+
+
+xelatex bibtex_7.tex
+bibtex bibtex_7.aux
+xelatex bibtex_7.tex
+xelatex bibtex_7.tex
+
---
+++ latex/ctex_study/bibliography_2.sh
@@ -0,0 +1,7 @@
+
+
+xelatex bibtex_7_2.tex
+bibtex bibtex_7_2.aux
+xelatex bibtex_7_2.tex
+xelatex bibtex_7_2.tex
+
